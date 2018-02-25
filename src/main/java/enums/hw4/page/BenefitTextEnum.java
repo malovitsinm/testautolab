c159bfda533933a22be06d6b6237d7ac98ea5cbe
malovitsinm <32491143+malovitsinm@users.noreply.github.com>
2018-02-25 21:08:35 +0300

    Missing enum files -> added

---
+++ src/main/java/enums/hw4/page/BenefitTextEnum.java
@@ -0,0 +1,15 @@
+package enums.hw4.page;
+
+public enum BenefitTextEnum {
+    IMG1("To include good practices and ideas from successful EPAM projec"),
+    IMG2("To be flexible and customizable"),
+    IMG3("To be multiplatform"),
+    IMG4("Already have good base (about 20 internal and some external projects), wish to get more…");
+
+    public String text;
+
+    BenefitTextEnum(String text) {
+        this.text = text;
+    }
+
+}
